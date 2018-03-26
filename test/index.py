f7844816c57c10e7e245ba6d7a067fb75d5d9ee3
gezhonghao <gezhonghao@hujiang.com>
2018-03-26 20:52:40 +0800

    1
    1

--- test/index.py
+++ test/index.py
@@ -36,5 +36,21 @@ print( [m + n for m in 'ABC' for n in 'XYZ'] ) 	         # ['AX', 'AY', 'AZ', 'B
 
 import os
 # print( [d for d in os.listdir('.')])
-for d in os.listdir('.')
+for d in os.listdir('.'):
 	print(d)
+
+listsort = sorted(['bob', 'about', 'Zoo', 'Credit'], key=str.lower, reverse=True)
+print(listsort) # ['Zoo', 'Credit', 'bob', 'about']
+
+def count1():
+	def num(i):   # 返回闭包时牢记一点：返回函数不要引用任何循环变量，或者后续会发生变化的变量。
+		def f():  # 如果一定要引用循环变量怎么办？方法是再创建一个函数，用该函数的参数绑定循环变量当前的值，无论该循环变量后续如何更改，已绑定到函数参数的值不变
+			return i*i
+		return f
+	fs = []
+	for item in range(1,4):
+		fs.append(num(item))
+	return fs
+f1, f2, f3 = count1()
+print(f1(),f2(),f3())
+
