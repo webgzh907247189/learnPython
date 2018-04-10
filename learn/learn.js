9adcdc9e91e12b6746e6e67b6f210267c6074f5b
webgzh907247189 <18255106634@163.com>
2018-04-11 02:28:36 +0800

    1
    1

--- learn/learn.js
+++ learn/learn.js
@@ -3,24 +3,71 @@ function reverseString(str) {
 }
 
 
+
 function filter(arr,cb){
-	return arr.filter(cb)
+	return arr.reduce((result,item)=>{
+		return result = item && [...result,item] || result
+	},[]).filter(cb)
 }
 
-function getElementsByClassName(){
-
+function filter(arr,cb){
+	return Array.from(arr,item => item && item || false).filter( item => item && item).filter(cb)
 }
+
+function filter(arr,cb){
+	return arr.filter( item => item && item).filter(cb)
+}
+
+
+
+function getElementsByClassName(HTMLElement,className){
+	return Array.from(document.querySelector(HTMLElement).querySelectorAll(className))
+}
+
+
 
 function getBoundObj(obj,arr){
+	Object.keys(obj).reduce((result,item)=>{
+		if(!result.includes(item)){
+			Reflect.deleteProperty(obj,item)
+		}
+		return result
+	},arr)
+	return obj
+}
+
+
+function getBoundObj(obj,arr){
+	let object = Object.create(null)
 	for(let item of arr){
-		obj[item]
+		object[item] = obj[item]
 	}
-	return 
+	return object
+}
+
+Object.defineProperty(target,key,{
+    get(){
+        return key.split('.').map((item,index)=>{
+            return target && (target = target[item]) || undefined
+        }).pop()
+    }
+})
+
+
+
+
+
+
+
+function sum(){
+	let result = Array.from(arguments,(item)=>{
+		return this.sumNumber += item
+	},{sumNumber: 0})
+	return function(){
+
+	}
 }
 
 function sum(){
-	Array.from(arguments,(item)=>{
-		return this.sumNumber += item
-	},{sumNumber: 0})
-	return sum()
+	return new Count()
 }
