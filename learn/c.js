02cc4d8fad1a304f262c151e36f4604aa45b0973
webgzh907247189 <18255106634@163.com>
2018-05-09 00:13:03 +0800

    1
    1

--- learn/c.js
+++ learn/c.js
@@ -1,6 +1,30 @@
-//2 函数节流
+//2 函数防抖
+{
+	let timer = null
+	function getDate(){
+		clearTimeout(timer)
 
+		timer = setTimeout(function(){
+			getList()
+		},500)
+	}
+}
+
+// 函数节流
+{
+	let isFetch = true
+	function getDate(){
+		if(!isFetch){
+			return
+		}
+
+		isFetch = false
+		setTimeout(function(){
+			getList()
+			isFetch = true
+		},500)
+	}
+}
 
 
 //代理模式
-// https://www.cnblogs.com/walls/p/6399837.html
