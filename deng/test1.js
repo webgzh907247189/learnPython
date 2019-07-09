fc03e36050652ce303e9430fc55fa420921da577
webgzh907247189 <18255106634@163.com>
2019-07-09 15:23:51 +0800

    promise

--- deng/test1.js
+++ deng/test1.js
@@ -227,6 +227,43 @@
 }
 
 
+{
+	const timeout = ms => new Promise((resolve, reject) => {        
+		setTimeout(() => {
+			resolve();
+		}, ms);
+	});
+	const ajax1 = () => timeout(2000).then(() => {
+        console.log("1");        
+        return 1;   
+    });
+    const ajax2 = () => timeout(1000).then(() => {        
+    	console.log("2");        
+    	return 2;    
+    }); 
+    const ajax3 = () => timeout(2000).then(() => {        
+    	console.log("3");        
+    	return 3;    
+	});
+	const mergePromise =  (ajaxArray) =>{
+		let arr = [] 
+
+		return ajaxArray.reduce((result,item)=>{
+			return result.then((d)=>{
+
+				return item().then((ff)=>{
+					arr = [...arr,ff]
+					return arr 
+				})
+			})
+		},Promise.resolve())
+	};
+	mergePromise([ajax1, ajax2, ajax3]).then(data => {    
+		console.log("done");
+		console.log(data);
+	});
+	// 执行结果为： 1  2  3 done [1,2,3] 
+}
 
 
 {	
